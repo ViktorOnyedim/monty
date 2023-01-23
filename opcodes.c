401cc00ee6a1d21d24d8c4a2e19c7743d97b2b93
ViktorOnyedim <viktoronyedim@gmail.com>
2023-01-23 00:26:43 +0000

    Added opcodes.c

---
+++ opcodes.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the instruction
+ * @arg: argument passed to the function
+ */
+
+void push(stack_t **stack, unsigned int line_number, char *arg)
+{
+
+}
