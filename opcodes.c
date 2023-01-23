41a126c64e9f34d8f61da08244340d245277699a
ViktorOnyedim <viktoronyedim@gmail.com>
2023-01-23 01:04:15 +0000

    Added push and pall functionalities

--- opcodes.c
+++ opcodes.c
@@ -9,5 +9,42 @@
 
 void push(stack_t **stack, unsigned int line_number, char *arg)
 {
+	stack_t *new;
 
+	if (arg == NULL)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	new = (stack_t *)malloc(sizeof(stack_t));
+
+	new -> n = atoi(arg);
+	new -> prev = NULL;
+	new -> next = *stack;
+
+	if (*stack != NULL)
+		(*stack) -> prev = new;
+	*stack = new;
+}
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the instruction
+ * @arg: argument passed to the instruction
+ */
+
+void pall(stack_t **stack, unsigned int line_number, char *arg)
+{
+	stack_t *temp;
+	temp = *stack;
+	(void)line_number;
+	(void)arg;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp -> n);
+		temp = temp -> next;
+	}
 }
